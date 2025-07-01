b36a50d945b14ff5b45ca45841d8ccee8ffd6b6f
Supong <butteredsupongporsche@gmail.com>
2025-07-01 11:30:51 +0700

    Initial Commit

---
+++ P2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  P2
+//
+//  Created by Porsche on 1/7/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("asdasdaqs")
+            Text("Hi")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P2/P2App.swift
@@ -0,0 +1,17 @@
+//
+//  P2App.swift
+//  P2
+//
+//  Created by Porsche on 1/7/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct P2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
