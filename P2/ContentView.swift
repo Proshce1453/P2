3673bb73209ea0af2256bfeed544e6fee4a8e877
Supong <butteredsupongporsche@gmail.com>
2025-07-01 11:54:03 +0700

    change logo

--- P2/ContentView.swift
+++ P2/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "arrowshape.up.circle.fill")
+            Image(systemName: "paperclip.circle.fill")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.yellow)
